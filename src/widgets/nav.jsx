4e4485bc0e0a4aa8fcce6d98cc8c23a57d09d0cd
Jedy <johnericssonytac@gmail.com>
2024-04-24 22:14:04 +0800

    Init pubish

---
+++ src/widgets/nav.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navi(){
+    return(
+        <>
+            <header>
+                <div className="iconAndName">
+                    <img src="" alt="icon" />
+                    <h1></h1>
+                </div>
+                <nav></nav>
+            </header>
+        </>
+    )
+}
+
+export default Navi
