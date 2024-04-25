731cb2575be57506f583c2cccf61c80917886ff0
Jedy <johnericssonytac@gmail.com>
2024-04-25 13:35:35 +0800

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,25 @@ function App() {
   return (
     <>
     	<header className="headerHome">
+        <img src="./src/assets/bgheader.png" alt="Header Background" className='bgimg'/>
 			  <Navi/>
         <div className="title">
           <div className="alge">Algebra</div>
           <div className="sub">Mastering Algebra: Prelims to Finals</div>
         </div>
 		  </header>
+      <div className='graphics'>
+        <img src="./src/assets/graphs.png" alt="graphics" className='graphs'/>
+      </div>
+      <div className='periods'>
+        <div className='periodIntro'>
+          <h1></h1>
+          <p></p>
+        </div>
+        <div className="semestrialterms">
+          
+        </div>
+      </div>
     </>
   )
 }
