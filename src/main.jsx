a535075ef5e603ccab212ab629b4ebfc967eddf3
Jedy <johnericssonytac@gmail.com>
2024-04-28 17:53:32 +0800

    changing routes

--- src/main.jsx
+++ src/main.jsx
@@ -8,51 +8,11 @@ import {
 import './index.css'
 
 import App from './App.jsx'
-import Prelims from './Prelim.jsx'
-import PrelimProblem from './Prelim_problems.jsx'
-import Midterms from './Midterm.jsx'
-import MidtermPronblem from './Midterm_problems.jsx'
-import Semis from './Semi.jsx'
-import SemiProblem from './Semis_problems.jsx'
-import Final from './Finals.jsx'
-import FinalProblem from './Finals_problem.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>
-  },
-  {
-    path: "/prelims",
-    element: <Prelims/>
-  },
-  {
-    path: "/prelim/:topicproblem",
-    element: <PrelimProblem/>
-  },
-  {
-    path: "/midterm",
-    element: <Midterms/>
-  },
-  {
-    path: "/midterm/:topicproblem",
-    element: <MidtermPronblem/>
-  },
-  {
-    path: "/semis",
-    element: <Semis/>
-  },
-  {
-    path: "/semis/:topicproblem",
-    element: <SemiProblem/>
-  },
-  {
-    path: "/finals",
-    element: <Final/>
-  },
-  {
-    path: "/finals/:topicproblems",
-    element: <FinalProblem/>
   }
 ])
 
