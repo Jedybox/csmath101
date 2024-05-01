e2dd8278bcbf4965c5abba9f45325290e6a6157f
Jedy <johnericssonytac@gmail.com>
2024-05-01 11:55:25 +0800

    smis problem page

--- src/pages/Semis/Semis_Problem.jsx
+++ src/pages/Semis/Semis_Problem.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import ErrorPage from "../Error/ErrorPage";
+
 function SemiProblem() {
-    return(<></>)
+    const topic = useParams()
+    const topics = ['LinearFunction', 'FunctionsRelations', 'QuadraticFunctions']
+
+    if (!topics.includes(topic.topic)) {
+        return(<ErrorPage/>)
+    } 
+        
+    return(
+        <>
+
+        </>
+    )
+    
 }
 
 export default SemiProblem
